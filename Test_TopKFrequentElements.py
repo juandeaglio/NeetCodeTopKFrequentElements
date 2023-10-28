4ea6fe14fc4541e59fd1bf09ec2284f3629e7b39
Anthony Volkov <45726108+juandeaglio@users.noreply.github.com>
2023-10-28 15:17:17 -0400

    Implemented a bucket sort algorithm too.

--- Test_TopKFrequentElements.py
+++ Test_TopKFrequentElements.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import Solution
+from main import Solution, HeapQueueTopKStrategy, BucketSortTopKStrategy
 
 
 class TopKFrequentElementsTest(unittest.TestCase):
@@ -18,6 +18,16 @@ class TopKFrequentElementsTest(unittest.TestCase):
         nums = [-1, -1]
         k = 1
         self.assertEqual([-1], Solution().topKFrequent(nums, k))
+
+    def test_non_trivial_case_heap_queue(self):
+        nums = [1, 1, 1, 2, 2, 3]
+        k = 2
+        self.assertEqual([1, 2], Solution(strategy=HeapQueueTopKStrategy).topKFrequent(nums, k))
+
+    def test_non_trivial_case_bucket_sort(self):
+        nums = [1, 1, 1, 2, 2, 3]
+        k = 2
+        self.assertEqual([1, 2], Solution(strategy=BucketSortTopKStrategy).topKFrequent(nums, k))
 
 if __name__ == '__main__':
     unittest.main()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List, Protocol
-
+import heapq
 
 class TopKStrategy(Protocol):
     @abstractmethod
@@ -20,8 +20,50 @@ class DictionaryTopKStrategy(TopKStrategy, ABC):
         return list(sorted_k_frequencies)
 
 
+class HeapQueueTopKStrategy(TopKStrategy, ABC):
+    def solve(self, nums: List[int], k: int) -> List[int]:
+        k_frequencies = {}
+
+        for num in nums:
+            k_frequencies[num] = k_frequencies.get(num, 0) + 1
+
+        min_heap = []
+        for key, val in k_frequencies.items():
+            heapq.heappush(min_heap, (val, key))
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+
+        sorted_heap = sorted(min_heap, reverse=True)
+        top_k_keys = []
+
+        for val, key in sorted_heap:
+            top_k_keys.append(key)
+
+        return top_k_keys
+
+
+class BucketSortTopKStrategy(TopKStrategy, ABC):
+    def solve(self, nums: List[int], k: int) -> List[int]:
+        k_frequencies = {}
+        buckets = [[] for _ in range(len(nums)+1)]
+        for num in nums:
+            k_frequencies[num] = k_frequencies.get(num, 0) + 1
+
+        for num, freq in k_frequencies.items():
+            buckets[freq].append(num)
+
+        top_k = []
+        for bucket in range(len(buckets)-1, -1, -1):
+            for val in buckets[bucket]:
+                top_k.append(val)
+                if len(top_k) == k:
+                    return top_k
+
+        return top_k
+
+
 class Solution:
-    def __init__(self, strategy=DictionaryTopKStrategy):
+    def __init__(self, strategy=BucketSortTopKStrategy):
         self.strategy = strategy()
 
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
